959c3408efcf4a807ff02c865f5c1e03539b915f
muna alshorman <munaalshorman123@gmail.com>
2020-01-21 03:33:10 +0200

    array shift

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # data-structures-and-algorithms
+
+# Array Shift
+
+
+### Author: muna
+
+### Links and Resources
+
+- [submission PR]
+- [ci/cd] (GitHub Actions)
+
+
+
+#### How to initialize/run your application (where applicable)
+
+-  `npm start`
+
+#### Tests
+
+- How do you run tests?
+- Any tests of note?
+- Describe any tests that you did not complete, skipped, etc
+
+#### UML
+
+Link to an image of the UML for your application and response to events
+![](UML.jpg)
---
+++ challenges /array-shift/array-shift.test.js
@@ -0,0 +1,17 @@
+const  insertShiftArray = require('../../array-shift');
+
+describe('I want to insert a value in the middle of an array (array.length is odd number)', ()=>{
+    it(' insert a value in the middle of an array',()=>{
+        insertShiftArray([4, 8, 15, 23, 42], 16)
+        expect(insertShiftArray[3]).toEqual(16)
+    })
+})
+
+describe('I want to insert a value in the middle of an array (array.length is even number)', ()=>{
+    it(' insert a value in the middle of an array',()=>{
+        insertShiftArray([2, 4, 6, 8], 5);
+        expect(insertShiftArray[2]).toEqual(16)
+    })
+})
+
+
