51ff32e5100e2f2da1ca244644884b7fea5201ee
muna alshorman <munaalshorman123@gmail.com>
2020-01-21 03:46:42 +0200

    array shift

--- challenges /array-shift/array-shift.test.js
+++ challenges /array-shift/array-shift.test.js
@@ -2,15 +2,16 @@ const  insertShiftArray = require('./array-shift.js');
 
 describe('I want to insert a value in the middle of an array (array.length is odd number)', ()=>{
     it(' insert a value in the middle of an array',()=>{
-        insertShiftArray([4, 8, 15, 23, 42], 16)
-        expect(insertShiftArray[3]).toEqual(16)
+        let array=[4, 8, 15, 23, 42]
+        insertShiftArray(array, 16)
+        expect(array[3]).toEqual(16)
     })
 })
 
 describe('I want to insert a value in the middle of an array (array.length is even number)', ()=>{
     it(' insert a value in the middle of an array',()=>{
         insertShiftArray([2, 4, 6, 8], 5);
-        expect(insertShiftArray[2]).toEqual(16)
+        expect(insertShiftArray([2, 4, 6, 8], 5)).toEqual([2, 4, 5, 6, 8])
     })
 })
 
