83a23761dc1b2e8351ad4ff142b8e5e948dd0d19
muna alshorman <munaalshorman123@gmail.com>
2020-01-26 14:43:28 +0200

    not completed

--- Data-Structures/linked-list.js
+++ Data-Structures/linked-list.js
@@ -1,15 +1,24 @@
 'use strict';
 //Create a Node class that has properties for the value stored in the Node, and a pointer to the next Node.
 class Node {
-  constructor(element){
-    this.element = element;
+  constructor(){
     this.head = head;
-    this.next = null;}
+  }
   insert(data){
-    let newNode = new Node(data);//  newNode is object 
-    newNode.next = this.head; // both (head and next)pointers point at the same node. 
-    this.head = newNode;// As we are inserting at the beginning the head pointer needs to now point at the newNode. 
-    return this.head;
+    let newNode = new Node(data);//  newNode is object
+
+    if (!this.head) {// As we are inserting at the beginning the head pointer needs to now point at the newNode.
+      this.head = newNode;
+    }
+    
+    let currentNode = this.head;
+    while(currentNode.next) {//if there is a next value
+      currentNode = currentNode.next
+    }
+
+    currentNode.next = newNode;
+    return this;
+  }
   };
   includes(val){
 
