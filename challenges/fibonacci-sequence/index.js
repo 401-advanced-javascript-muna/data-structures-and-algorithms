2696d9bc213e236636c1696e1755af18e29753c7
muna alshorman <munaalshorman123@gmail.com>
2020-01-28 10:17:37 +0200

    fix eslint

--- challenges/fibonacci-sequence/index.js
+++ challenges/fibonacci-sequence/index.js
@@ -1,15 +1,15 @@
 'use strict';
 // module.exports =fibonacciSequenceArray;
 let fibonacciSequence = module.exports = {};
-fibonacciSequence.fibonacciSequenceArray=(n)=> {
+fibonacciSequence.fibonacciSequenceArray = (n)=> {
 
-let array = [0,1] ;
+  let array = [0,1] ;
 
-for (let i=2 ; i<=n; i++ ){
-  array.push(array[i-1]+ array[i-2]) ;
+  for (let i = 2 ; i <= n; i++ ){
+    array.push(array[i - 1] + array[i - 2]) ;
 
-}
-return array;
+  }
+  return array;
 
-}
+};
 
--- challenges/fibonacci-sequence/index.test.js
+++ challenges/fibonacci-sequence/index.test.js
@@ -1,11 +1,11 @@
 const  fibo = require('./index.js');
 
 describe('', ()=>{
-    it(' insert a number ',()=>{
-        
-              
-        expect(fibo.fibonacciSequenceArray(5)).toEqual([ 0, 1, 1, 2, 3, 5 ])
-    })
-})
+  it(' insert a number ',()=>{
+
+
+    expect(fibo.fibonacciSequenceArray(5)).toEqual([ 0, 1, 1, 2, 3, 5 ]);
+  });
+});
 
 
