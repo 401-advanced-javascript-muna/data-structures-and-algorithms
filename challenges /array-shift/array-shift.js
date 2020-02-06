17ed3a408f5d1e6ddcf6e57f178faa7eeda49045
muna alshorman <munaalshorman123@gmail.com>
2020-02-06 14:01:09 +0200

    eslint fixed

--- challenges /array-shift/array-shift.js
+++ challenges /array-shift/array-shift.js
@@ -1,18 +1,19 @@
+/* eslint-disable strict */
 'use strict';
 function insertShiftArray(arr, val) {
-    let middle = Math.ceil(arr.length / 2);
-    
-    let newArray = [];
+  let middle = Math.ceil(arr.length / 2);
+
+  let newArray = [];
 
 
-    for (let i = 0; i <= arr.length; ++i) {
-        if (i < middle){newArray[i] = arr[i]; }
+  for (let i = 0; i <= arr.length; ++i) {
+    if (i < middle){newArray[i] = arr[i]; }
 
-        if (i === middle){newArray[i] = val;}
+    if (i === middle){newArray[i] = val;}
 
-        if (i > middle){newArray[i] = arr[i - 1];}
-    }
-    return newArray;
+    if (i > middle){newArray[i] = arr[i - 1];}
+  }
+  return newArray;
 }
 
 module.exports = insertShiftArray;
--- challenges /array-shift/array-shift.test.js
+++ challenges /array-shift/array-shift.test.js
@@ -1,19 +1,19 @@
 const  insertShiftArray = require('./array-shift.js');
 
 describe('I want to insert a value in the middle of an array (array.length is odd number)', ()=>{
-    it(' insert a value in the middle of an array',()=>{
-        
-        let array=[4, 8, 15, 23, 42]
-        
-        expect(insertShiftArray(array, 16)).toEqual([4, 8, 15, 16, 23, 42])
-    })
-})
+  it(' insert a value in the middle of an array',()=>{
+
+    let array = [4, 8, 15, 23, 42];
+
+    expect(insertShiftArray(array, 16)).toEqual([4, 8, 15, 16, 23, 42]);
+  });
+});
 
 describe('I want to insert a value in the middle of an array (array.length is even number)', ()=>{
-    it(' insert a value in the middle of an array',()=>{
-        insertShiftArray([2, 4, 6, 8], 5);
-        expect(insertShiftArray([2, 4, 6, 8], 5)).toEqual([2, 4, 5, 6, 8])
-    })
-})
+  it(' insert a value in the middle of an array',()=>{
+    insertShiftArray([2, 4, 6, 8], 5);
+    expect(insertShiftArray([2, 4, 6, 8], 5)).toEqual([2, 4, 5, 6, 8]);
+  });
+});
 
 
