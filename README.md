a109651a2fd14d042b0dd8167284b89b0d89389a
muna alshorman <munaalshorman123@gmail.com>
2020-02-13 20:54:04 +0200

    not completed

--- README.md
+++ README.md
@@ -7,6 +7,8 @@
 
 ## Challenge Description
 
+breadth first traversal method which takes a Binary Tree as its unique input. Without utilizing any of the built-in methods available to your language, traverse the input tree using a Breadth-first approach, and return a list of the values in the tree in the order they were encountered.
+
 
 ## Approach & Efficiency
 - add O(logn)
@@ -15,8 +17,7 @@
 
 - inOrder(),postOrder(),preOrder()  : O(n)
 
-
+- breadthFirstTrevarsal(tree) : o(n)
 ## Solution
-![](wb151.jpg)
-![](wb152.jpg)
+![](wl.jpg)
 
---
+++ challenges/breadth-first/breadth-first.js
@@ -0,0 +1,26 @@
+'use strict ';
+
+
+module.exports = function breadthFirst(tree){
+
+
+    console.log('dddddddddddd',tree,tree.root.val);
+
+  let queue ;
+  if (tree.root === null){return null;}
+  else{
+    array.push(tree.root);
+    // console.log('array',array);
+
+        // while(array.length !== 0){
+        //   let node = array.pop();
+        //   console.log('node ',node);
+        //   if (node.left !== null){array.push(node.left.val);}
+        //   if (node.right !== null){array.push(node.right.val);}
+        // }
+        // console.log('array',array);
+        // return array;
+
+  }
+
+};
